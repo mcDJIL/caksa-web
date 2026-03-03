bb6fba710ce8ac2a54c2e75f69eb25ec2c84b983
mcDJIL <mdjauharil29@gmail.com>
2026-03-03 08:40:18 +0700

    update sponsor djarum foundation

--- src/components/sections/Partner/PlatinumSupport.tsx
+++ src/components/sections/Partner/PlatinumSupport.tsx
@@ -59,6 +59,50 @@ const PlatinumSupport = () => {
             </a>
           </div>
         </div>
+        <div className="flex-1 max-w-2xl">
+          <img
+            src={Clev}
+            alt="Clev Innovation Labs"
+            className="bg-center bg-cover w-96 rounded-2xl mx-auto"
+            loading="lazy"
+          />
+          <h1 className="w-full mt-5 text-2xl font-bold text-center">
+            Clev Innovation Labs
+          </h1>
+          <p className="text-xl text-center pt-7">
+            Clev Innovation Labs adalah Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius voluptatibus necessitatibus esse asperiores, modi repellat ea fugit tenetur quis voluptate laboriosam veritatis magni, architecto velit, rerum unde ut accusantium non..{' '}
+          </p>
+          <div className="flex justify-center w-full pt-8 font-poppins">
+            <a
+              href="#"
+              target="_blank"
+              rel="noreferrer"
+              onMouseEnter={() => setOnHover(true)}
+              onMouseLeave={() => setOnHover(false)}
+              className="border-2 border-[#F58A07] text-[#F58A07] rounded-md px-4 py-4 flex items-center gap-4  hover:bg-[#F58A07] focus:ring-4 focus:outline-white-100 focus:ring-white-300 hover:text-white"
+            >
+              <p>Company Profile</p>
+              <span>
+                <svg
+                  className={classNames(
+                    onHover ? 'rotate-45' : 'rotate-0',
+                    'transition-all duration-100 ease-linear'
+                  )}
+                  width="16"
+                  height="16"
+                  viewBox="0 0 11 11"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.8253 1.01732C10.8253 0.741182 10.6015 0.517325 10.3253 0.517325L5.82533 0.517325C5.54918 0.517325 5.32533 0.741183 5.32533 1.01732C5.32533 1.29347 5.54918 1.51732 5.82533 1.51732L9.82533 1.51732L9.82533 5.51732C9.82533 5.79347 10.0492 6.01732 10.3253 6.01732C10.6015 6.01732 10.8253 5.79347 10.8253 5.51732L10.8253 1.01732ZM1.1569 10.8929L10.6789 1.37088L9.97177 0.663772L0.449791 10.1858L1.1569 10.8929Z"
+                    fill={onHover ? '#FFFFFF' : '#F58A07'}
+                  />
+                </svg>
+              </span>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/data/home.constant.ts
+++ src/data/home.constant.ts
@@ -3,7 +3,7 @@ export const carousel = [
   { alt: 'PT YPTI Toolcraft', src: '/assets/img/carousel/pt-ypti.jpg' },
   { alt: 'Buaya Aerotech', src: '/assets/img/carousel/buaya-aerotech.png' },
   { alt: 'QProp', src: '/assets/img/carousel/qprop.png' },
-  { alt: 'Logo Place', src: '/assets/img/carousel/logo-ipsum.png' },
+  { alt: 'Djarum Foundation', src: '/assets/img/carousel/djarum-foundation.png' },
   { alt: 'Logo Place', src: '/assets/img/carousel/logo.png' },
   { alt: 'Logo Place', src: '/assets/img/carousel/logo-ipsum.png' },
 ];
